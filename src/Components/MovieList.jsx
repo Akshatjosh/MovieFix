18566b4ac25008f48e80a1b93a13106db0d8fc5c
akshatjosh <akshatjoshi34471956@gmail.com>
2024-08-17 15:19:56 +0530

    added some css

--- src/Components/MovieList.jsx
+++ src/Components/MovieList.jsx
@@ -4,7 +4,7 @@ function MovieList({ title, movies }) {
   return (
     <div className="px-6">
       <h1 className="text-lg md:text-3xl py-4 text-white">{title}</h1>
-      <div className="flex overflow-x-auto">
+      <div className="flex overflow-x-auto overflow-y-hidden ">
         <div className="flex">
           {movies?.map((movie) => (
             <MovieCard
